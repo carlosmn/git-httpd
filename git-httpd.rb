ce69108021fe3f8b49c28e5cfd3a329f4d562cdd
Carlos Martín Nieto <carlos@cmartin.tk>
2012-04-09 06:10:59 +0200

    Use inline if and unless

--- git-httpd.rb
+++ git-httpd.rb
@@ -13,14 +13,11 @@ repo = Rugged::Repository.new(REPO_PATH)
 get '*' do |path|
   commit = repo.lookup(repo.ref(REF_NAME).target)
   path.slice!(0)
-  if path.empty?
-    path = 'index.html'
-  end
+  path = 'index.html' if path.empty?
+
   puts path
   content = repo.file_at(commit.oid, path)
-  if content.nil?
-    halt 404, "404 Not Found"
-  end
+  halt 404, "404 Not Found" unless content
 
   content_type MIME::Types.type_for(path).first.content_type
   content
